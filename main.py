3f88653df0c34e82bf5c5b766dae07458cfd2170
Rafael Carvalho <rrafaelcarvalhoj@gmail.com>
2024-03-25 19:57:11 -0300

    refactor: From FastAPI to FLASK

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
-from fastapi import FastAPI
+from flask import Flask, request, jsonify
 
-app = FastAPI()
+app = Flask(__name__)
 
+@app.route("/")
+def root():
+    return jsonify({"nimbus": "judge"})
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
+@app.route("/judge", methods=["POST"])
+def judge():
+    body = request.form.to_dict()
+    f = request.files['file']
+    #f.save(f.filename)
+    return jsonify({
+        "body": body,
+        "file": {
+            "name": f.filename.split(".")[0],
+            "extention": f.filename.split(".")[1],
+            "size": str(len(f.read())),
+            "size_type": "bytes"
+        }
+    })
