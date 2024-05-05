517fea2bf0af7e1efeda77655385df289ab664eb
Rafael Carvalho <rrafaelcarvalhoj@gmail.com>
2024-05-05 19:09:13 -0300

    build: chalice build requiriments

---
+++ chalicelib/JUDGE_MicroJudge/Judge/build-and-test.sh
@@ -0,0 +1,344 @@
+#!/bin/bash
+
+function LOG()
+{
+  echo "$*" >&2
+}
+
+if [[ ! -n "$3" ]]; then
+  echo "$0 <LANGUAGE> <SRCFILE> <PROBLEMTEMPLATEDIR> <RUNALL?yes:no>"
+  exit 1
+fi
+
+LANGUAGE=$1
+SRCCODE=$(realpath $2)
+PROBLEMTEMPLATEDIR=$(realpath $3)
+RUNALL=$4
+RUNALL=${RUNALL:=no}
+
+workdirbase=$(mktemp -d)
+workdir=$workdirbase/cagefiles/
+
+if [[ ! -e "$workdirbase" ]]; then
+  echo "Could not create $workdirbase"
+  exit 1
+fi
+
+exec 2> $workdirbase/build-and-test.log
+
+LOG "% Running: $(basename $PROBLEMTEMPLATEDIR)"
+LOG ""
+LOG "- Minimal Information"
+LOG "  - Submission Language: $LANGUAGE"
+LOG "  - Submission SRCFILE: $(basename $SRCCODE)"
+LOG "  - Run all even on critical error: $RUNALL"
+LOG ""
+LOG ""
+
+mkdir -p $workdir
+echo $workdirbase
+
+cd $(dirname $0)
+
+cp "$SRCCODE" $workdir/
+
+declare -a BIN
+
+declare -A ULIMITS
+declare -A TLMOD
+
+DEFAULTSHIELDCPU=3
+DEFAULTSHIELDUSER=judge
+DEFAULTMEMLIMIT=600
+
+#set default values
+#configurações de variáveis do ulimit
+## stack
+### default to 200MB
+ULIMITS[-s]=204800
+
+## file size
+### default to 100MB
+ULIMITS[-f]=256000
+
+## virtual memory
+### default to 600MB
+#ULIMITS[-v]=614400
+
+## max processes
+### default to 1024
+ULIMITS[-u]=1024
+
+#check if there is conf
+[[ -e $PROBLEMTEMPLATEDIR/conf ]] && source $PROBLEMTEMPLATEDIR/conf
+
+LOG "## LIMITS via ulimits"
+LOG ""
+#set ulimits
+for l in ${!ULIMITS[@]}; do
+  ulimit $l ${ULIMITS[$l]}
+  LOG "set: ulimit $l ${ULIMITS[$l]}"
+done
+LOG ""
+LOG ""
+
+#default locations
+LANGUAGEDIR=lang/$LANGUAGE
+PROBLEMLANGUAGEDIR="$PROBLEMTEMPLATEDIR/scripts/$LANGUAGE"
+DEFAULTLANGUAGEDIR=$(realpath $LANGUAGEDIR)
+#LANGCOMPILE=$(realpath $LANGCOMPILE)
+
+#search for special compile script for problem
+#if it does not exist defaults to default language compile
+LANGCOMPILE=$PROBLEMLANGUAGEDIR/compile.sh
+[[ ! -e "$LANGCOMPILE" ]] && LANGCOMPILE="$DEFAULTLANGUAGEDIR/compile.sh"
+
+if [[ ! -e "$LANGCOMPILE" ]]; then
+  echo "Language '$LANGUAGE' not availale"
+  LOG "Language '$LANGUAGE' not availale"
+  LOG "$LANGCOMPILE not found"
+  exit 3
+fi
+
+PREPLANGUAGE="$PROBLEMLANGUAGEDIR/prep.sh"
+[[ -x "$PREPLANGUAGE" ]] && $PREPLANGUAGE $workdir
+[[ ! -e "$PREPLANGUAGE" ]] && [[ -e "$DEFAULTLANGUAGEDIR/prep.sh" ]] &&
+  $DEFAULTLANGUAGEDIR/prep.sh $workdir
+
+if [[ "$USER" == root ]]; then
+  SHIELDPARAMS="--shield-cpu $DEFAULTSHIELDCPU --shield-user $DEFAULTSHIELDUSER -M $DEFAULTMEMLIMIT"
+fi
+
+LOG "# Compiling code"
+LOG ""
+bash cage-run.sh -w $workdir -r $LANGCOMPILE $SHIELDPARAMS\
+                    -s $workdirbase/compile.log.stderr \
+                    -o $workdirbase/compile.log.stdout \
+                    -t $workdirbase/compile.log.time \
+                    -T 30\
+                    -B $workdirbase/compile.log.bwraptime &> $workdirbase/compile.log.cage-run
+
+CAGERET=$?
+
+if ! grep -q ^BIN= $workdirbase/compile.log.stdout || (( CAGERET != 0 )) ; then
+  LOG "   COMPILATION ERROR"
+  LOG ""
+  LOG "CE $workdir"
+  for f in $workdirbase/compile.log.{stdout,stderr,cage-run,time,bwraptime}; do
+    LOG "## $f"
+    LOG "$(< $f)"
+    LOG ""
+  done
+  echo "Compilation Error"
+  exit 1
+fi
+#cut -d'=' -f2 < $workdir/log.stdout
+BIN+=( $(cut -d'=' -f2 < $workdirbase/compile.log.stdout) )
+echo BIN=${BIN[0]} > $workdir/binfile.sh
+
+LOG ""
+LOG ""
+LOG "# Running test files"
+
+#search for runner script, first in problem second default
+LANGRUN=$PROBLEMLANGUAGEDIR/run.sh
+[[ ! -e "$LANGRUN" ]] && LANGRUN=$DEFAULTLANGUAGEDIR/run.sh
+
+#Search for compare script, first: language specific in problem
+# second: common in problem
+# third: language specific default
+# fourth: commom default
+LANGCOMPARE=$PROBLEMLANGUAGEDIR/compare.sh
+[[ ! -e "$LANGCOMPARE" ]] && LANGCOMPARE=$PROBLEMTEMPLATEDIR/scripts/compare.sh
+[[ ! -e "$LANGCOMPARE" ]] && LANGCOMPARE=$DEFAULTLANGUAGEDIR/compare.sh
+[[ ! -e "$LANGCOMPARE" ]] && LANGCOMPARE=$(realpath lang/compare.sh)
+#LANGCOMPARE=$(realpath $LANGCOMPARE)
+
+#[[ -e "$PROBLEMTEMPLATEDIR/scripts/$LANGUAGE/compare.sh" ]] && LANGCOMPARE="$PROBLEMTEMPLATEDIR/scripts/$LANGUAGE/compare.sh"
+
+if [[ ! -e "$LANGRUN" ]]; then
+  echo "Language '$LANGUAGE' not availale"
+  LOG "$LANGRUN not found"
+  exit 3
+fi
+
+if [[ ! -e "$LANGCOMPARE" ]]; then
+  echo "Language '$LANGUAGE' not availale"
+  LOG "$LANGCOMPARE not found"
+  exit 3
+fi
+
+declare -A TL
+if [[ ! -e "$PROBLEMTEMPLATEDIR/tl" ]]; then
+  echo "Wrong package format. No TimeLimit found"
+  LOG "Timelimit is not set"
+  exit 3
+fi
+source $PROBLEMTEMPLATEDIR/tl
+
+[[ ! -n "${TL[$LANGUAGE]}" ]] && TL[$LANGUAGE]=${TL[default]}
+
+if [[ -n "${TLMOD[$LANGUAGE.sum]}" ]]; then
+  TL[$LANGUAGE]=$(echo "${TL[$LANGUAGE]}+${TLMOD[$LANGUAGE.sum]}"|bc -l)
+fi
+if [[ -n "${TLMOD[$LANGUAGE.mult]}" ]]; then
+  TL[$LANGUAGE]=$(echo "${TL[$LANGUAGE]}*${TLMOD[$LANGUAGE.mult]}"|bc -l)
+fi
+
+if [[ ! -n "${TLMOD[$LANGUAGE.drift]}" ]]; then
+  TLMOD[$LANGUAGE.drift]=0
+fi
+
+ETL=$(echo 2+${TL[$LANGUAGE]}+0.2+${TLMOD[$LANGUAGE.drift]}*2|bc -l)
+RESP=""
+RESPERRO=0
+CORRECT=0
+TOTALTESTS=$(ls -d $PROBLEMTEMPLATEDIR/tests/input/*|wc -l)
+
+function run-testinput()
+{
+  local INPUT=$1
+  local FILE=$(basename $INPUT)
+  bash cage-run.sh -d $workdir -i $INPUT -o $workdirbase/$FILE-team_output \
+       -s $workdirbase/$FILE-stderr $SHIELDPARAMS\
+       -r $LANGRUN \
+       -t $workdirbase/$FILE-log.timelog\
+       -T $ETL\
+       -B $workdirbase/$FILE-log.bwraptime &> $workdirbase/$FILE-log.cage-run
+  BWRAPEXITCODE=$?
+  echo $BWRAPEXITCODE > $workdirbase/$FILE-log.bwrapexitcode
+}
+
+JOBSCOUNT=0
+NPROC=$(nproc)
+[[ "$ALLOWPARALLELTEST" == "n" ]] && NPROC=1 && LOG " - Parallel Test not allowed in this problem"
+LOG " - NPROC: $NPROC"
+for INPUT in $PROBLEMTEMPLATEDIR/tests/input/*; do
+  if [[ ! -e "$INPUT" ]]; then
+    echo "Wrong package format. No input found"
+    LOG "$INPUT not found"
+    exit 3
+  fi
+  run-testinput $INPUT &
+  ((JOBSCOUNT++))
+  if (( JOBSCOUNT > NPROC-1 )); then
+      wait -n
+      ((JOBSCOUNT--))
+  fi
+done
+
+wait
+
+TLERERUN=${TLERERUN:=y}
+
+for INPUT in $PROBLEMTEMPLATEDIR/tests/input/*; do
+  LOG "--------------------------------------------------------------------"
+  if [[ ! -e "$INPUT" ]]; then
+    echo "Wrong package format. No input found"
+    LOG "$INPUT not found"
+    exit 3
+  fi
+  FILE=$(basename $INPUT)
+  LOG ""
+  LOG "## Testfile: $FILE"
+  LOG ""
+  THISRERUN=0
+  EXECTIME=$(grep '^real' $workdirbase/$FILE-log.timelog|awk '{print $NF}')
+  if [[ "$ALLOWPARALLELTEST" != "n" ]] && echo "($EXECTIME - ${TL[$LANGUAGE]}) > ${TLMOD[$LANGUAGE.drift]} "|bc -l |grep -q 1; then
+      if [[ "$TLERERUN" == "y" ]]; then
+	  LOG " - Rerun: because got TLE while running parallel tests"
+	  run-testinput $INPUT
+	  THISRERUN=1
+      else
+	  LOG " - Rerun: It will not be RERUNNED because previous RERUN were TLE"
+      fi
+  fi
+  LOG ""
+  LOG "### CAGE CONTROL DATA this is for Bruno to check"
+  LOG "8<-------------------------8<------------------"
+  for f in $workdirbase/$FILE-{stderr,log.cage-run,log.timelog,log.bwraptime,log.bwrapexitcode}; do
+    wc -c "$f"|grep -q "^0 " && continue;
+    [[ "$f" == "$workdirbase/$FILE-team_output" ]] && continue
+    LOG "#### $(basename $f)"
+    LOG "$(< $f)"
+  done
+  LOG "8<-------------------------8<------------------"
+  LOG "### END CAGE CONTROL DATA"
+  LOG ""
+  LOG ""
+  SMALLRESP=none
+  BWRAPEXITCODE=$(< $workdirbase/$FILE-log.bwrapexitcode)
+  EXECTIME=$(grep '^real' $workdirbase/$FILE-log.timelog|awk '{print $NF}')
+  if echo "($EXECTIME - ${TL[$LANGUAGE]}) > ${TLMOD[$LANGUAGE.drift]} "|bc -l |grep -q 1; then
+    OLDRESP="$RESP"
+    [[ "$RESP" != "Runtime Error" ]] && RESP="Time Limit Exceeded"
+    SMALLRESP=TLE
+    LOG "- $FILE TLE $EXECTIME > ${TL[$LANGUAGE]}"
+    ((RESPERRO++))
+    ((THISRERUN==1)) && TLERERUN=n
+  fi
+
+  if (( BWRAPEXITCODE != 0 )) && [[ "$SMALLRESP" != "TLE" ]] && ! grep -q signal $workdirbase/$FILE-log.timelog; then
+    [[ "$RESP" != "Runtime Error" ]] && RESP="Runtime Error - Signaled PPID"
+    SMALLRESP=TMT
+    ((RESPERRO++))
+    [[ ! -n "$EXECTIME" ]] && EXECTIME="$(grep '^real' $workdirbase/$FILE-log.bwraptime|awk '{print $NF}')"
+  elif (( BWRAPEXITCODE != 0 )) && ( [[ "$SMALLRESP" != "TLE" ]] || grep -q signal $workdirbase/$FILE-log.timelog ); then
+    RESP="Runtime Error"
+    #LOG "- $FILE Runtime Error"
+    SMALLRESP=RE
+    ((RESPERRO++))
+  fi
+
+  if [[ "$SMALLRESP" == "none" ]]; then
+    $LANGCOMPARE $workdirbase/$FILE-team_output $PROBLEMTEMPLATEDIR/tests/output/$FILE $INPUT &> $workdirbase/$FILE-log.compare
+    COMPAREEXIT=$?
+    LOG "### CHECKING SOLUTION THIS IS USUALLY A DIFF OUTPUT"
+    if (( COMPAREEXIT == 4 )); then
+      if (( RESPERRO == 0 )); then
+        RESP="Accepted"
+      fi
+      SMALLRESP=AC
+      ((CORRECT++))
+    elif (( COMPAREEXIT == 5 )); then
+      if ((RESPERRO == 0 )) ; then
+        RESP="Accepted,PE"
+      fi
+      SMALLRESP=AC,PE
+      ((CORRECT++))
+    elif (( COMPAREEXIT == 6 )); then
+      [[ "$RESP" != "Time Limit Exceeded" ]] && [[ "$RESP" != "Runtime Error" ]] && RESP="Wrong Answer"
+      SMALLRESP=WA
+      ((RESPERRO++))
+    else
+      RESP="Unknown Error"
+      SMALLRESP=UE
+    ((RESPERRO++))
+    fi
+  else
+    LOG " - Not checking answer because of $SMALLRESP"
+  fi
+  #LOG " - Test Veredict: $SMALLRESP ($RESP)"
+
+
+  LOG "EXECTIME $FILE $EXECTIME $SMALLRESP"
+  LOG " - Execution Time: $EXECTIME"
+  LOG " - Time Limit for this problem is: ${TL[$LANGUAGE]}"
+  LOG " - Veredict for this output: $SMALLRESP"
+  LOG ""
+  ((RESPERRO > 2 )) && [[ "$SMALLRESP" != "AC" ]] && [[ "$SMALLRESP" != "AC,PE" ]] && LOG " - Will NOT show DIFFS or Courtesy for MORE than 2 errors"
+  ((RESPERRO <= 2 )) && [[ "$SMALLRESP" != "AC" ]] && [[ "$SMALLRESP" != "TMT" ]] && [[ "$SMALLRESP" != "TLE" ]] && [[ "$SMALLRESP" != "RE" ]] && LOG "$(< $workdirbase/$FILE-log.compare)"
+  ((RESPERRO <= 2 )) && [[ "$SMALLRESP" != "AC,PE" ]] && [[ "$SMALLRESP" != "AC" ]] && [[ "$INPUT" =~ "sample" || "$INPUT" =~ "example" ]] && LOG "" && LOG "#### INPUT COURTESY [this is the raw input file]" && LOG "\`\`\`" && LOG "$(< $INPUT)" && LOG "\`\`\`" && LOG ""
+  LOG ""
+  [[ "$RESP" != "Accepted" ]] && [[ "$RESP" != "Accepted,PE" ]] && [[ "$RESP" != "Presentation Error" ]] && [[ "$RUNALL" == "no" ]]  && break
+
+done
+
+LOG ""
+LOG ""
+LOG "# FINAL VEREDICT"
+LOG "  - $RESP"
+LOG "  - $CORRECT correct in $TOTALTESTS , $((CORRECT*100/TOTALTESTS))%"
+
+echo "$RESP,$((CORRECT*100/TOTALTESTS))p"
+exit 0
---
+++ chalicelib/JUDGE_MicroJudge/Judge/fatorial/scripts/c/compile.sh
@@ -0,0 +1,718 @@
+#!/bin/bash
+
+cat <<EOF >/tmp/rwdir/mainfunction$RANDOM$RANDOM.c
+#include <stdio.h>
+
+double cos (double x){
+  printf("Bad function name: cos\n");
+  abort();
+}
+
+double sin(double x){
+  printf("Bad function name: sin\n");
+  abort();
+}
+
+double tan(double x){
+  printf("Bad function name: tan\n");
+  abort();
+}
+
+double acos(double x){
+  printf("Bad function name: acos\n");
+  abort();
+}
+
+double asin(double x){
+  printf("Bad function name: asin\n");
+  abort();
+}
+
+double atan(double x){
+  printf("Bad function name: atan\n");
+  abort();
+}
+
+double atan2(double y, double x){
+  printf("Bad function name: atan2\n");
+  abort();
+}
+
+double cosh(double x){
+  printf("Bad function name: cosh\n");
+  abort();
+}
+
+double sinh(double x){
+  printf("Bad function name: sinh\n");
+  abort();
+}
+
+double tanh(double x){
+  printf("Bad function name: tanh\n");
+  abort();
+}
+
+double acosh(double x){
+  printf("Bad function name: acosh\n");
+  abort();
+}
+
+float acoshf (float x){
+  printf("Bad function name: acoshf\n");
+  abort();
+}
+
+long double acoshl (long double x){
+  printf("Bad function name: acoshl\n");
+  abort();
+}
+
+double asinh(double x){
+  printf("Bad function name: asinh\n");
+  abort();
+}
+
+float asinhf (float x){
+  printf("Bad function name: asinhf\n");
+  abort();
+}
+
+long double asinhl (long double x){
+  printf("Bad function name: asinhl\n");
+  abort();
+}
+
+double atanh(double x){
+  printf("Bad function name: atanh\n");
+  abort();
+}
+
+float atanhf (float x){
+  printf("Bad function name: atanhf\n");
+  abort();
+}
+
+long double atanhl (long double x){
+  printf("Bad function name: atanhl\n");
+  abort();
+}
+
+double exp(double x){
+  printf("Bad function name: exp\n");
+  abort();
+}
+
+double frexp (double x, int* exp){
+  printf("Bad function name: frexp\n");
+  abort();
+}
+
+double ldexp(double x, int exp){
+  printf("Bad function name: ldexp\n");
+  abort();
+}
+
+double log(double x){
+  printf("Bad function name: log\n");
+  abort();
+}
+
+double log10(double x){
+  printf("Bad function name: log\n");
+  abort();
+}
+
+double modf(double x, double* intpart){
+  printf("Bad function name: modf\n");
+  abort();
+}
+
+double exp2(double x){
+  printf("Bad function name: exp2 \n");
+  abort();
+}
+
+float exp2f (float x){
+  printf("Bad function name: exp2f\n");
+  abort();
+}
+
+long double exp2l (long double x){
+  printf("Bad function name: exp2l\n");
+  abort();
+}
+
+double expm1(double x){
+  printf("Bad function name: expm1 \n");
+  abort();
+}
+
+float expm1f (float x){
+  printf("Bad function name: expm1f\n");
+  abort();
+}
+
+long double expm1l (long double x){
+  printf("Bad function name: expm1l\n");
+  abort();
+}
+
+double ilogb(double x){
+  printf("Bad function name: ilogb \n");
+  abort();
+}
+
+float ilogbf (float x){
+  printf("Bad function name: ilogbf\n");
+  abort();
+}
+
+long double ilogbl (long double x){
+  printf("Bad function name: ilogbl\n");
+  abort();
+}
+
+double log1p(double x){
+  printf("Bad function name: log1p \n");
+  abort();
+}
+
+float log1pf (float x){
+  printf("Bad function name: log1pf\n");
+  abort();
+}
+
+long double log1pl (long double x){
+  printf("Bad function name: log1pl\n");
+  abort();
+}
+
+double log2(double x){
+  printf("Bad function name: log1p \n");
+  abort();
+}
+
+float log2f (float x){
+  printf("Bad function name: log1pf\n");
+  abort();
+}
+
+long double log2l (long double x){
+  printf("Bad function name: log1pl\n");
+  abort();
+}
+
+double logb(double x){
+  printf("Bad function name: logb \n");
+  abort();
+}
+
+float logbf (float x){
+  printf("Bad function name: logbf\n");
+  abort();
+}
+
+long double logbl (long double x){
+  printf("Bad function name: logbl\n");
+  abort();
+}
+
+double scalbn(double x     , int n){
+  printf("Bad function name: scalbn \n");
+  abort();
+}
+
+float scalbnf (float x      , int n){
+  printf("Bad function name: scalbnf\n");
+  abort();
+}
+
+long double scalbnl (long double x, int n){
+  printf("Bad function name: scalbnl\n");
+  abort();
+}
+
+double scalbln(double x     , long int n){
+  printf("Bad function name: scalbln \n");
+  abort();
+}
+
+float scalblnf (float x      , long int n){
+  printf("Bad function name: scalblnf\n");
+  abort();
+}
+
+long double scalblnl (long double x, long int n){
+  printf("Bad function name: scalblnl\n");
+  abort();
+}
+
+double pow  (double base     , double exponent){
+  printf("Bad function name: pow \n");
+  abort();
+}
+
+float powf (float base      , float exponent){
+  printf("Bad function name: powf \n");
+  abort();
+}
+
+long double powl (long double base, long double exponent){
+  printf("Bad function name: powl \n");
+  abort();
+}
+
+double sqrt  (double x){
+  printf("Bad function name: sqrt \n");
+  abort();
+}
+
+float sqrtf (float x){
+  printf("Bad function name: sqrtf \n");
+  abort();
+}
+
+long double sqrtl (long double x){
+  printf("Bad function name: sqrtl \n");
+  abort();
+}
+
+double cbrt  (double x){
+  printf("Bad function name: cbrt \n");
+  abort();
+}
+
+float cbrtf  (float x){
+  printf("Bad function name: cbrtf  \n");
+  abort();
+}
+
+long double cbrtl  (long double x){
+  printf("Bad function name: cbrtl  \n");
+  abort();
+}
+
+double hypot  (double x     , double y){
+  printf("Bad function name: hypot \n");
+  abort();
+}
+
+float hypotf (float x      , float y){
+  printf("Bad function name: hypotf  \n");
+  abort();
+}
+
+long double hypotl (long double x, long double y){
+  printf("Bad function name: hypotl  \n");
+  abort();
+}
+
+double erf  (double x){
+  printf("Bad function name: erf \n");
+  abort();
+}
+
+float erff (float x){
+  printf("Bad function name: erff  \n");
+  abort();
+}
+
+long double erfl (long double x){
+  printf("Bad function name: erfl  \n");
+  abort();
+}
+
+double erfc (double x){
+  printf("Bad function name: erfc\n");
+  abort();
+}
+
+float erfcf (float x){
+  printf("Bad function name: erfcf\n");
+  abort();
+}
+
+long double erfcl (long double x){
+  printf("Bad function name: erfcl\n");
+  abort();
+}
+
+double tgamma (double x){
+  printf("Bad function name: tgamma\n");
+  abort();
+}
+
+float tgammaf (float x){
+  printf("Bad function name: tgammaf\n");
+  abort();
+}
+
+long double tgammal (long double x){
+  printf("Bad function name: tgammal\n");
+  abort();
+}
+
+double lgamma (double x){
+  printf("Bad function name: lgamma\n");
+  abort();
+}
+
+float lgammaf (float x){
+  printf("Bad function name: lgammaf\n");
+  abort();
+}
+
+long double lgammal (long double x){
+  printf("Bad function name: lgammal\n");
+  abort();
+}
+
+double ceil (double x){
+  printf("Bad function name: ceil\n");
+  abort();
+}
+
+float ceilf (float x){
+  printf("Bad function name: ceilf\n");
+  abort();
+}
+
+long double ceill (long double x){
+  printf("Bad function name: ceill\n");
+  abort();
+}
+
+double floor (double x){
+  printf("Bad function name: floor\n");
+  abort();
+}
+
+float floorf (float x){
+  printf("Bad function name: floorf\n");
+  abort();
+}
+
+long double floorl (long double x){
+  printf("Bad function name: floorl\n");
+  abort();
+}
+
+double fmod  (double numer     , double denom){
+  printf("Bad function name: fmod\n");
+  abort();
+}
+
+float fmodf (float numer      , float denom){
+  printf("Bad function name: fmodf\n");
+  abort();
+}
+
+long double fmodl (long double numer, long double denom){
+  printf("Bad function name: fmodl\n");
+  abort();
+}
+
+double trunc (double x){
+  printf("Bad function name: trunc\n");
+  abort();
+}
+
+float truncf (float x){
+  printf("Bad function name: truncf\n");
+  abort();
+}
+
+long double truncl (long double x){
+  printf("Bad function name: truncl\n");
+  abort();
+}
+
+double round (double x){
+  printf("Bad function name: round\n");
+  abort();
+}
+
+float roundf (float x){
+  printf("Bad function name: roundf\n");
+  abort();
+}
+
+long double roundl (long double x){
+  printf("Bad function name: roundl\n");
+  abort();
+}
+
+long int lround  (double x){
+  printf("Bad function name: lround\n");
+  abort();
+}
+
+long int lroundf (float x){
+  printf("Bad function name: lroundf\n");
+  abort();
+}
+
+long int lroundl (long double x){
+  printf("Bad function name: lroundl\n");
+  abort();
+}
+
+long long int llround  (double x){
+  printf("Bad function name: llround\n");
+  abort();
+}
+
+long long int llroundf (float x){
+  printf("Bad function name: llroundf\n");
+  abort();
+}
+
+long long int llroundl (long double x){
+  printf("Bad function name: llroundl\n");
+  abort();
+}
+
+double rint (double x){
+  printf("Bad function name: rint\n");
+  abort();
+}
+
+float rintf (float x){
+  printf("Bad function name: rintf\n");
+  abort();
+}
+
+long double rintl (long double x){
+  printf("Bad function name: rintl\n");
+  abort();
+}
+
+long int lrint (double x){
+  printf("Bad function name: lrint\n");
+  abort();
+}
+
+long int lrintf (float x){
+  printf("Bad function name: lrintf\n");
+  abort();
+}
+
+long int lrintl (long double x){
+  printf("Bad function name: lrintl\n");
+  abort();
+}
+
+long long int llrint (double x){
+  printf("Bad function name: llrint\n");
+  abort();
+}
+
+long long int llrintf (float x){
+  printf("Bad function name: llrintf\n");
+  abort();
+}
+
+long long int llrintl (long double x){
+  printf("Bad function name: llrintl\n");
+  abort();
+}
+
+double nearbyint (double x){
+  printf("Bad function name: nearbyint\n");
+  abort();
+}
+
+float nearbyintf (float x){
+  printf("Bad function name: nearbyintf\n");
+  abort();
+}
+
+long double nearbyintl (long double x){
+  printf("Bad function name: nearbyintl\n");
+  abort();
+}
+
+double remainder  (double numer     , double denom){
+  printf("Bad function name: remainder\n");
+  abort();
+}
+
+float remainderf (float numer      , float denom){
+  printf("Bad function name: remainderf\n");
+  abort();
+}
+
+long double remainderl (long double numer, long double denom){
+  printf("Bad function name: remainderl\n");
+  abort();
+}
+
+double remquo  (double numer     , double denom     , int* quot){
+  printf("Bad function name: remquo\n");
+  abort();
+}
+
+float remquof (float numer      , float denom      , int* quot){
+  printf("Bad function name: remquof\n");
+  abort();
+}
+
+long double remquol (long double numer, long double denom, int* quot){
+  printf("Bad function name: remquol\n");
+  abort();
+}
+
+double copysign  (double x     , double y){
+  printf("Bad function name: copysign\n");
+  abort();
+}
+
+float copysignf (float x      , float y){
+  printf("Bad function name: copysignf\n");
+  abort();
+}
+
+long double copysignl (long double x, long double y){
+  printf("Bad function name: copysignl\n");
+  abort();
+}
+
+double nan (const char* tagp){
+  printf("Bad function name: nan\n");
+  abort();
+}
+
+double nextafter  (double x     , double y){
+  printf("Bad function name: nextafter\n");
+  abort();
+}
+
+float nextafterf (float x      , float y){
+  printf("Bad function name: nextafterf\n");
+  abort();
+}
+
+long double nextafterl (long double x, long double y){
+  printf("Bad function name: nextafterl\n");
+  abort();
+}
+
+double nexttoward  (double x     , long double y){
+  printf("Bad function name: nexttoward\n");
+  abort();
+}
+
+float nexttowardf (float x      , long double y){
+  printf("Bad function name: nexttowardf\n");
+  abort();
+}
+
+long double nexttowardl (long double x, long double y){
+  printf("Bad function name: nexttowardl\n");
+  abort();
+}
+
+double fdim  (double x     , double y){
+  printf("Bad function name: fdim\n");
+  abort();
+}
+
+float fdimf (float x      , float y){
+  printf("Bad function name: fdimf\n");
+  abort();
+}
+
+long double fdiml (long double x, long double y){
+  printf("Bad function name: fdiml\n");
+  abort();
+}
+
+double fmax  (double x     , double y){
+  printf("Bad function name: fmax\n");
+  abort();
+}
+
+float fmaxf (float x      , float y){
+  printf("Bad function name: fmaxf\n");
+  abort();
+}
+
+long double fmaxl (long double x, long double y){
+  printf("Bad function name: fmaxl\n");
+  abort();
+}
+
+double fmin  (double x     , double y){
+  printf("Bad function name: fmin\n");
+  abort();
+}
+
+float fminf (float x      , float y){
+  printf("Bad function name: fminf\n");
+  abort();
+}
+
+long double fminl (long double x, long double y){
+  printf("Bad function name: fminl\n");
+  abort();
+}
+
+double fabs (double x){
+  printf("Bad function name: fabs\n");
+  abort();
+}
+
+float fabsf (float x){
+  printf("Bad function name: fabsf\n");
+  abort();
+}
+
+long double fabsl (long double x){
+  printf("Bad function name: fabsl\n");
+  abort();
+}
+
+double fma  (double x     , double y     , double z){
+  printf("Bad function name: fma\n");
+  abort();
+}
+
+float fmaf (float x      , float y      , float z){
+  printf("Bad function name: fmaf\n");
+  abort();
+}
+
+long double fmal (long double x, long double y, long double z){
+  printf("Bad function name: fmal\n");
+  abort();
+}
+
+
+int fatorial(int n);
+
+int main(){
+  int n;
+
+  scanf("%d", &n);
+  printf("%d\n", fatorial(n));
+  
+  return 0;
+}
+EOF
+
+exec 2>/tmp/stderrlog > /tmp/out
+cd /tmp/rwdir
+
+cat > Makefile << 'EOF'
+
+SRC=$(wildcard *.c)
+CFLAGS=-lm -O2 -static
+
+main: ${SRC}
+	@gcc ${CFLAGS} $^ -o $@ -lm
+	@echo BIN=$@
+EOF
+
+unset MAKELEVEL
+make
