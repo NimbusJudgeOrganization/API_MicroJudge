15a09e7099d2c8dfe6037a6f94488ffc737c8ca8
Rafael Carvalho <rrafaelcarvalhoj@gmail.com>
2024-05-23 22:00:56 -0300

    feat: API to submit base64 programs

--- app.py
+++ app.py
@@ -1,21 +1,48 @@
 import sys
 import os
+from subprocess import Popen, PIPE
+import json
+import base64
+
+def cmdline(command):
+    process = Popen(
+        args=command,
+        stdout=PIPE,
+        shell=True
+    )
+    return process.communicate()[0]
+
 def handler(event, context):
-    print('Hello from AWS Lambda using Python' + sys.version + '!')
-    print('-> ls')
-    os.system('ls')
-    print('-> ls judge')
-    os.system('ls judge')
-    # print('-> calibreitor')
-    # os.system('/judge/./calibreitor.sh /judge/fatorial')
-    print('-> build-and-test')
-    os.system('bash judge/build-and-test.sh c judge/fat.c judge/fatorial')
-    #print('-> gcc -lm -O2 -static jorge.c -o /tmp/rwdir/main')
-    #os.system('cd /tmp/rwdir && gcc -lm -O2 -static jorge.c -o main')
-    #print('-> ls /tmp/rwdir')
-    #os.system('ls /tmp/rwdir')
-    #print('-> cat /tmp/rwdir/test-016-team_output')
-    #os.system('cat /tmp/rwdir/test-016-team_output')
-    #print('-> cat /tmp/rwdir/build-and-test.log')
-    #os.system('cat /tmp/rwdir/build-and-test.log')
-    return 'ok'
+    try:
+        question = event['question']
+        encoded_base64_submition = event['encoded_base64_submition']
+        file_name = event['file_name']
+        
+        # Decodifique o conteúdo base64
+        file_content = base64.b64decode(encoded_base64_submition)
+        print(file_content)
+        
+        temp_file_path = f'/tmp/{file_name}'
+        with open(temp_file_path, 'wb') as f:
+            f.write(file_content)
+        
+        build_and_test_output = cmdline(f'bash judge/build-and-test.sh c /tmp/{file_name} judge/{question}').decode('utf-8')
+        
+        lines = build_and_test_output.strip().split('\n')
+        status = lines[-1].split(',')[0]
+        percentage = int(lines[-1].split(',')[1].replace('p', ''))
+        
+        response = {
+            'statusCode': 200,
+            'body': json.dumps({
+                'status': status,
+                'percentage': percentage
+            })
+        }
+        
+        return response
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': json.dumps(f"Error: {str(e)}")
+        }
