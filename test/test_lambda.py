24f61529cb5f626c7d6d3a7b7483e246163f29d9
Rafael Carvalho <rrafaelcarvalhoj@gmail.com>
2024-06-08 07:23:52 -0300

    test: reesctruct tests

--- test/test_lambda.py
+++ test/test_lambda.py
@@ -1,27 +1,13 @@
 from lambda_utils import invoke_lambda
 
 
-def test_fatorial_AC1():
+def test_AC():
     assert invoke_lambda('fatorial/good/jorge.c', 'fatorial') == "Accepted"
-
-
-def test_fatorial_AC2():
     assert invoke_lambda('fatorial/good/jorge.c', 'fatorial') == "Accepted"
-
-
-def test_primos_arrojados_AC1():
     assert invoke_lambda('primos_arrojados/good/ribas-ac.c', 'primos_arrojados') == "Accepted"
 
 
-def primos_arrojados_TL1():
+def test_TL():
     assert invoke_lambda('primos_arrojados/slow/john-tle.c', 'primos_arrojados') == "Time Limit Exceeded"
-
-
-def test_primos_arrojados_TL2():
     assert invoke_lambda('primos_arrojados/slow/ribas-tle.c', 'primos_arrojados') == "Time Limit Exceeded"
-
-
-def test_primos_arrojados_TL3():
     assert invoke_lambda('primos_arrojados/slow/ribas2-tle.c', 'primos_arrojados') == "Time Limit Exceeded"
-
-
